094b9c3ee580bcbd30122bb894399dba4e39b227
Harshad Sharma <narsinhaenggworks@gmail.com>
2025-09-22 18:07:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: set 'base' to '/<your-repo-name>/' if deploying to GitHub Pages
-// e.g., base: '/narsinha-sales-pwa/'
+// IMPORTANT: base must match your GitHub repo name exactly
+// In your case the repo is "SaleRecords"
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/SaleRecords/',
 })
